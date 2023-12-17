b83d426ffc22dc29d13549c8f36ce9a193c262ee
Parth <parthbangde123@gmail.com>
2023-12-17 11:16:46 +0530

    Update dependencies and add dark mode support

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -11,7 +11,37 @@ export const authSlice = createSlice({
 	name: "auth",
 	initialState,
 	reducers: {
-		
+		setMode: (state) => {
+			state.mode = state.mode === 'light' ? 'dark' : 'light';
+		},
+		setLogin: (state, action) => {
+			state.user = action.payload.user;
+			state.token = action.payload.token;
+		},
+		setLogout: (state) => {
+			state.user = null;
+			state.token = null;
+		},
+		setFriends: (state, action) => {
+			if (state.user) {
+				state.user.friends = action.payload.friends;
+			} else {
+				console.log('User has no friends :<');
+			}
+		},
+		setPosts: (state, action) => {
+			state.posts = action.payload.posts;
+		},
+		setPost: (state, action) => {
+			const updatedPost = state.post.map((post) => {
+				if (post._id === action.payload.post_id) return action.payload.post_id;
+				return post;
+			})
+			state.posts = updatedPost;
+		}
 	}
 });
+
+export const { setPost, setFriends, setLogin, setLogout, setMode, setPosts } = authSlice.actions;
+export default authSlice.reducer;
 
