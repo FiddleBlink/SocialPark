1727ff5877f4cbc34f1fcdf95edf62d6fed3e68a
Parth <parthbangde123@gmail.com>
2024-01-14 12:30:15 +0530

    added advert widget and friendlist widget

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -35,17 +35,34 @@ export const addremoveFriend = async (req, res) => {
 		const user = await User.findById(id);
 		const friend = await User.findById(friendID);
 
+			
 		if (user.friends.includes(friendID)) {
-			user.friends = user.friends.filter((id) => id !== friendID);
-			friend.friends = friend.friends.filter((id) => id !== id);
+			if (id === friendID) { 
+				user.friends = user.friends.filter((id) => id !== friendID);
+			} else {
+				user.friends = user.friends.filter((id) => id !== friendID);
+				friend.friends = friend.friends.filter((id) => id !== id);
+			}
 		} else {
-			user.friends.push(friendID);
-			friend.friends.push(id);
+			if (id === friendID) { 
+				user.friends.push(friendID);
+			} else {		
+				user.friends.push(friendID);
+				friend.friends.push(id);
+			}
 		};
+
+		console.log(user.friends);
+		console.log(friend.friends);
+
 		await user.save();
 		await friend.save();
 
+		console.log("Saved");
+		
 		const friends = await Promise.all(user.friends.map((id) => User.findById(id)));
+
+		console.log(friends);
 
         const formattedFriends = friends.map(
             ({ _id, firstName, lastName, occupation, location, picturePath }) => {
